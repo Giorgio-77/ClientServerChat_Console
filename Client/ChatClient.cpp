155df7a228729de057080549924d7f15be806919
Yurii Krutsko <yuri.krutsko@gmail.com>
2021-11-18 21:59:10 +0300

    Release candidate

--- Client/ChatClient.cpp
+++ Client/ChatClient.cpp
@@ -4,7 +4,53 @@
 #include "../Lib/Client.h"
 #pragma comment(lib, "ws2_32.lib")
 
+using namespace std;
+using namespace Socket;
+
+Client* chat_client = nullptr;
+
+void signalHandler(int);
+void printMessage(const string&);
+
 int main(int argc, char** argv) {
 
+	signal(SIGINT, signalHandler);
+
+	cout << "Configure Client..." << endl;
+	// IP
+	string server = "127.0.0.1";
+
+	// Server listening port 
+	int port = 5000;
+	cout << "Port: ";
+	cin >> port;
+
+	try {
+		chat_client = new Client(server, port);
+		chat_client->setOnMessage(printMessage);
+		chat_client->connectServer();
+		delete chat_client;
+	}
+	catch (const exception& e) {
+		cerr << e.what() << endl;
+	}
+
+	system("pause");
 	return 0;
+}
+
+void signalHandler(int code) {
+	char ch;
+	cout << "Are you sure you want to close socket?(Y/N)";
+	cin >> ch;
+	if (toupper(ch) == 'Y' && chat_client != nullptr) {
+		delete chat_client;
+		exit(0);
+	}
+	cin.clear();
+	cin.ignore(32767, '\n');
+}
+
+void printMessage(const string& t_message) {
+	std::cout << t_message << std::endl;
 }
--- Sever/ChatServer.cpp
+++ Sever/ChatServer.cpp
@@ -4,8 +4,54 @@
 #include "../Lib/Server.h"
 #pragma comment(lib, "ws2_32.lib")
 
+using namespace std;
+using namespace Socket;
+
+Server* chat_server = nullptr;
+
+void signalHandler(int);
+void printMessage(const string&);
 
 int main(int argc, char** argv) {
 
+	signal(SIGINT, signalHandler);
+
+	cout << "Configure Server..." << endl;
+	// port
+	int port = 5000;
+	cout << "Port: ";
+	cin >> port;
+
+	// Number of connections
+	int connection_size = 16;
+	cout << "Connections: ";
+	cin >> connection_size;
+
+	try {
+		chat_server = new Server(port, connection_size);
+		chat_server->setOnMessage(printMessage);
+		chat_server->runServer();
+		delete chat_server;
+	}
+	catch (const exception& e) {
+		cerr << e.what() << endl;
+	}
+
 	return 0;
+}
+
+void signalHandler(int code) {
+	char ch;
+	cout << "Are you sure you want to close socket?(Y/N)";
+	cin >> ch;
+	if (toupper(ch) == 'Y' && chat_server != nullptr) {
+		delete chat_server;
+		exit(0);
+	}
+	cin.clear();
+	cin.ignore(32767, '\n');
+}
+
+void printMessage(const string& t_message) {
+	std::cout << "> " << t_message << std::endl;
 }
