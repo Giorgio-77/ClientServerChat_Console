076ea9a291ea2504db99633de9d182d57a8c7c95
Yurii Krutsko <yuri.krutsko@gmail.com>
2021-11-18 17:23:57 +0300

    NodeV4.h released

--- Lib/NodeV4.h
+++ Lib/NodeV4.h
@@ -1,5 +1,72 @@
 #pragma once
+#ifndef NODEV4_H
+#define NODEV4_H
 
-class NodeV4
-{
-};
+#include <cstring>
+#include <stdexcept>
+
+// cross platform
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+typedef int socklen_t;
+static WSAData wsaData;
+#else
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#endif
+
+namespace Socket {
+
+	enum class STATE {
+		OPEN,
+		CLOSED
+	};
+
+	class NodeV4 {
+	protected:
+		// Socket
+		int m_socketFd = -1;
+		struct sockaddr_in m_socketInfo;
+		int m_port = 5000;
+		int m_addressLen = 0;
+		int m_messageSize = 4096;
+		STATE m_state;
+
+		// socket
+		void setSocketInfo();
+		void createSocket();
+		void sendMessage(const int& t_socketFd, const std::string& t_message);
+		void closeSocket();
+
+	public:
+		NodeV4();
+		NodeV4(const int& t_port);
+
+		// Port
+		int getPort() const;
+		void setPort(const int& t_port);
+
+		// Socket file descriptor
+		int getSocketFd() const;
+		void setSocketFd(const int& t_socketFd);
+
+		// Socket information
+		sockaddr_in getSocketInfo() const;
+		void setSocketInfo(const sockaddr_in& t_socketInfo);
+
+		// Connection status
+		STATE getState() const;
+		void setState(const STATE& t_state);
+
+		// Message size
+		int getMessageSize() const;
+		void setMessageSize(const int& t_messageSize);
+
+		virtual ~NodeV4();
+	};
+
+}
+
+#endif /* NODEV4_H */
